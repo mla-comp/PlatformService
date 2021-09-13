cc845b67dff69afa89d8c407df950916b3969ace
Michael Lauritsen <i3028841@gmail.com>
2021-09-13 22:05:21 +0200

    Add DbContext

--- Startup.cs
+++ Startup.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PlatformService.Data;
+using PlatformService.Models;
 
 namespace PlatformService
 {
@@ -26,7 +29,9 @@ namespace PlatformService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppDbContext>(opt => 
+                opt.UseInMemoryDatabase("InMem"));
+                
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -34,7 +39,6 @@ namespace PlatformService
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
