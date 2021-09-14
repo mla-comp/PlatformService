eee11a24c6a7ee44e56c8c8ea7454a900bf32704
Michael Lauritsen <mla@complyto.com>
2021-09-14 07:21:25 +0200

    Add Missing cost prop

--- Data/PrepDb.cs
+++ Data/PrepDb.cs
@@ -24,7 +24,7 @@ namespace PlatformService.Data
                 context.Platforms.AddRange(
                     new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
                     new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
-                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation" }
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
                 );
 
                 context.SaveChanges();
