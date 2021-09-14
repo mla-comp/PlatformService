16e0f9d500dedbf0956fd2b88d307c9d4b31f060
Michael Lauritsen <mla@complyto.com>
2021-09-14 07:03:51 +0200

    Add Automapper profiles

---
+++ Profiles/PlatformProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformProfile : Profile
+    {
+        public PlatformProfile()
+        {
+            // Source to target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+    }
+}
